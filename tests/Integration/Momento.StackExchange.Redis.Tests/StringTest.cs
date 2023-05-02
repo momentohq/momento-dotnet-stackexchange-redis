0647c65b37e2604dc0781bf17dc33677bc4f8d52
Michael Landis <michael@momentohq.com>
2023-05-02 12:11:18 -0700

    test: add integration tests for async methods
    Adds integration tests for corresponding async methods. Also tests
    `StringSet` expiration.

--- tests/Integration/Momento.StackExchange.Redis.Tests/StringTest.cs
+++ tests/Integration/Momento.StackExchange.Redis.Tests/StringTest.cs
@@ -22,9 +22,28 @@ public class StringTest : TestBase
     }
 
     [Fact]
+    public async Task GetAsync_MissingCache_ThrowsException()
+    {
+        if (useRedis)
+        {
+            return;
+        }
+
+        var db = new MomentoRedisDatabase(client!, Utils.GuidString());
+        await Assert.ThrowsAsync<RedisServerException>(() => db.StringGetAsync(Utils.GuidString()));
+    }
+
+    [Fact]
     public void Get_MissingKey_HappyPath()
     {
         var result = db.StringGet(Utils.GuidString());
+        Assert.True(result.IsNull);
+    }
+
+    [Fact]
+    public async Task GetAsync_MissingKey_HappyPath()
+    {
+        var result = await db.StringGetAsync(Utils.GuidString());
         Assert.True(result.IsNull);
     }
 
@@ -38,6 +57,52 @@ public class StringTest : TestBase
 
         var storedValue = db.StringGet(key);
         Assert.Equal(value, storedValue);
+    }
+
+    [Fact]
+    public async Task SetAsync_NoExpiry_HappyPath()
+    {
+        var key = Utils.GuidString();
+        var value = Utils.GuidString();
+        var result = await db.StringSetAsync(key, value, null, When.Always);
+        Assert.True(result);
+
+        var storedValue = await db.StringGetAsync(key);
+        Assert.Equal(value, storedValue);
+    }
+
+    [Fact]
+    public void SetAsync_HasExpiryAndExpires_HappyPath()
+    {
+        var key = Utils.GuidString();
+        var value = Utils.GuidString();
+        var result = db.StringSet(key, value, TimeSpan.FromSeconds(5), When.Always);
+        Assert.True(result);
+
+        var storedValue = db.StringGet(key);
+        Assert.Equal(value, storedValue);
+
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+
+        storedValue = db.StringGet(key);
+        Assert.True(storedValue.IsNull);
+    }
+
+    [Fact]
+    public async Task SetAsync_HasExpiryAndExpires_HappyPathAsync()
+    {
+        var key = Utils.GuidString();
+        var value = Utils.GuidString();
+        var result = await db.StringSetAsync(key, value, TimeSpan.FromSeconds(5), When.Always);
+        Assert.True(result);
+
+        var storedValue = await db.StringGetAsync(key);
+        Assert.Equal(value, storedValue);
+
+        await Task.Delay(TimeSpan.FromSeconds(5));
+
+        storedValue = await db.StringGetAsync(key);
+        Assert.True(storedValue.IsNull);
     }
 
     [Fact]
@@ -67,6 +132,32 @@ public class StringTest : TestBase
     }
 
     [Fact]
+    public async Task KeyDeleteAsync_MissingKey_HappyPath()
+    {
+        var key = Utils.GuidString();
+
+        // Verify key not there
+        var getResponse = await db.StringGetAsync(key);
+        Assert.True(getResponse.IsNull);
+
+        // Delete
+        var result = await db.KeyDeleteAsync(Utils.GuidString());
+
+        if (useRedis)
+        {
+            Assert.False(result);
+        }
+        else
+        {
+            Assert.True(result);
+        }
+
+        // Verify key still not there
+        getResponse = await db.StringGetAsync(key);
+        Assert.True(getResponse.IsNull);
+    }
+
+    [Fact]
     public void KeyDelete_KeyExists_HappyPath()
     {
         var key = Utils.GuidString();
@@ -83,6 +174,26 @@ public class StringTest : TestBase
 
         // Verify key is gone
         getResponse = db.StringGet(key);
+        Assert.True(getResponse.IsNull);
+    }
+
+    [Fact]
+    public async Task KeyDeleteAsync_KeyExists_HappyPath()
+    {
+        var key = Utils.GuidString();
+        var value = Utils.GuidString();
+        var result = await db.StringSetAsync(key, value, null, When.Always);
+        Assert.True(result);
+
+        // Verify key is there
+        var getResponse = await db.StringGetAsync(key);
+        Assert.Equal(value, getResponse);
+
+        var deleteResponse = await db.KeyDeleteAsync(key);
+        Assert.True(deleteResponse);
+
+        // Verify key is gone
+        getResponse = await db.StringGetAsync(key);
         Assert.True(getResponse.IsNull);
     }
 }
